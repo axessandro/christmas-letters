b5d9aada2a945d695f47eda22e0671bbdc5e25b8
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2023-01-10 09:18:53 +0100

    Edit page rounded input

--- resources/views/letters/edit.blade.php
+++ resources/views/letters/edit.blade.php
@@ -9,10 +9,10 @@
             {{-- First name --}}
             <div class="d-flex align-itmes-center w-100 mb-4">
                 <label class="w-25" for="name">
-                    <h3>First name:</h3>
+                    <h4>First name:</h4>
                 </label>
-                <input value="{{ old('name', $letter->name) }}" class="w-75" type="text" id="name" name="name"
-                    class="form-control  @error('name') is-invalid @enderror">
+                <input value="{{ old('name', $letter->name) }}" type="text" id="name" name="name"
+                    class="form-control w-75 rounded-5 px-3  @error('name') is-invalid @enderror">
                 @error('name')
                     <div class="invalid-feedback d-block w-100 text-center">
                         {{ $message }}
@@ -24,10 +24,10 @@
             {{-- Last name --}}
             <div class="d-flex align-itmes-center w-100 mb-4">
                 <label class="w-25" for="surname">
-                    <h3>Last name:</h3>
+                    <h4>Last name:</h4>
                 </label>
-                <input value="{{ old('surname', $letter->surname) }}" class="w-75" type="text" id="surname"
-                    name="surname" class="form-control @error('surname') is-invalid @enderror">
+                <input value="{{ old('surname', $letter->surname) }}" type="text" id="surname" name="surname"
+                    class="form-control w-75 rounded-5 px-3 @error('surname') is-invalid @enderror">
                 @error('surname')
                     <div class="invalid-feedback d-block w-100 text-center">
                         {{ $message }}
@@ -39,10 +39,10 @@
             {{-- Address --}}
             <div class="d-flex align-itmes-center w-100 mb-4">
                 <label class="w-25" for="address">
-                    <h3> Address:</h3>
+                    <h4> Address:</h4>
                 </label>
-                <input value="{{ old('address', $letter->address) }}" class="w-75" type="text" id="address"
-                    name="address" class="form-control @error('address') is-invalid @enderror">
+                <input value="{{ old('address', $letter->address) }}" type="text" id="address" name="address"
+                    class="form-control w-75 rounded-5 px-3 @error('address') is-invalid @enderror">
                 @error('address')
                     <div class="invalid-feedback d-block w-100 text-center">
                         {{ $message }}
@@ -54,10 +54,10 @@
             {{-- City --}}
             <div class="d-flex align-itmes-center w-100 mb-4">
                 <label class="w-25" for="city">
-                    <h3>City:</h3>
+                    <h4>City:</h4>
                 </label>
-                <input value="{{ old('city', $letter->city) }}" class="w-75" type="text" id="city" name="city"
-                    class="form-control @error('city') is-invalid @enderror">
+                <input value="{{ old('city', $letter->city) }}" type="text" id="city" name="city"
+                    class="form-control w-75 rounded-5 px-3 @error('city') is-invalid @enderror">
                 @error('city')
                     <div class="invalid-feedback d-block w-100 text-center">
                         {{ $message }}
@@ -69,10 +69,11 @@
             {{-- Arrival date --}}
             <div class="d-flex align-itmes-center w-100 mb-4">
                 <label class="w-25" for="arrival_date">
-                    <h3>Arrival date:</h3>
+                    <h4>Arrival date:</h4>
                 </label>
-                <input value="{{ old('arrival_date', $letter->arrival_date) }}" class="w-75" type="date"
-                    id="arrival_date" name="arrival_date" class="form-control @error('arrival_date') is-invalid @enderror">
+                <input value="{{ old('arrival_date', $letter->arrival_date) }}" type="date" id="arrival_date"
+                    name="arrival_date"
+                    class="form-control w-75 rounded-5 px-3 @error('arrival_date') is-invalid @enderror">
                 @error('arrival_date')
                     <div class="invalid-feedback d-block w-100 text-center">
                         {{ $message }}
@@ -84,10 +85,10 @@
             {{-- Gift --}}
             <div class="d-flex align-itmes-center w-100 mb-4">
                 <label class="w-25" for="gift">
-                    <h3>Gift:</h3>
+                    <h4>Gift:</h4>
                 </label>
-                <input value="{{ old('gift', $letter->gift) }}" class="w-75" type="text" id="gift" name="gift"
-                    class="form-control @error('gift') is-invalid @enderror">
+                <input value="{{ old('gift', $letter->gift) }}" type="text" id="gift" name="gift"
+                    class="form-control rounded-5 px-3 w-75 @error('gift') is-invalid @enderror">
                 @error('gift')
                     <div class="invalid-feedback d-block w-100 text-center">
                         {{ $message }}
@@ -99,10 +100,10 @@
             {{-- Text --}}
             <div class="d-flex align-itmes-center w-100 mb-4">
                 <label class="w-25" for="text">
-                    <h3>Text:</h3>
+                    <h4>Text:</h4>
                 </label>
-                <textarea name="text" id="text" class="form-control @error('text') is-invalid @enderror w-75" cols="30"
-                    rows="3">{{ old('text', $letter->text) }}</textarea>
+                <textarea name="text" id="text" class="form-control rounded-3 px-3 @error('text') is-invalid @enderror w-75"
+                    cols="30" rows="3">{{ old('text', $letter->text) }}</textarea>
                 @error('text')
                     <div class="invalid-feedback d-block w-100 text-center">
                         {{ $message }}
@@ -114,10 +115,10 @@
             {{-- Kid rating --}}
             <div class="d-flex align-itmes-center w-100 mb-4">
                 <label class="w-25" for="kid_rating">
-                    <h3>Kid rating:</h3>
+                    <h4>Kid rating:</h4>
                 </label>
                 <select name="kid_rating" id="kid_rating"
-                    class="form-select @error('kid_rating') is-invalid @enderror w-75">
+                    class="form-select rounded-5 px-3 @error('kid_rating') is-invalid @enderror w-75">
                     <option @selected(old('kid_rating', $letter->kid_rating) == 1) value="1">Very bad</option>
                     <option @selected(old('kid_rating', $letter->kid_rating) == 2) value="2">Bad</option>
                     <option @selected(old('kid_rating', $letter->kid_rating) == 3) value="3">Normal</option>
@@ -135,9 +136,10 @@
             {{-- Delivered --}}
             <div class="d-flex align-itmes-center w-100 mb-4">
                 <label class="w-25" for="deliverd">
-                    <h3>Delivered:</h3>
+                    <h4>Delivered:</h4>
                 </label>
-                <select name="delivered" id="delivered" class="form-select @error('delivered') is-invalid @enderror w-75">
+                <select name="delivered" id="delivered"
+                    class="form-select rounded-5 px-3 @error('delivered') is-invalid @enderror w-75">
                     <option @selected(old('delivered', $letter->delivered) == 0) value="0">No</option>
                     <option @selected(old('delivered', $letter->delivered) == 1) value="1">Yes</option>
                 </select>
